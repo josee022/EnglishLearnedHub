10eafa58ef54d63b2c8a63e76ab9f16065775c77
josee022 <jose.mondelo@iesdonana.org>
2025-06-18 17:16:27 +0200

    navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -62,18 +62,6 @@ export const Navbar: React.FC = () => {
                 </>
               ) : (
                 <>
-                  <Link 
-                    to="/about" 
-                    className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                  >
-                    Acerca de
-                  </Link>
-                  <Link 
-                    to="/features" 
-                    className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                  >
-                    Características
-                  </Link>
                 </>
               )}
             </div>
